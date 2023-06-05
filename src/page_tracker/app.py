55a15b5472a9dabfcdc04fbbf3c30efb40e9d8ee
Ray Oh <raymond.oh@gmail.com>
2023-06-05 15:07:03 -0400

    created unit test for app

--- src/page_tracker/app.py
+++ src/page_tracker/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from redis import Redis
+from functools import cache
 
 app = Flask(__name__)
 redis = Redis()
 
 @app.get("/")
 def index():
-  page_views = redis.incr("page_views")
+  page_views = redis().incr("page_views")
   return f"This page has been viewed {page_views} times."
+
+@cache
+def redis():
+  return Redis()
