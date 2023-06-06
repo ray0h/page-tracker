1fdb3540e5b0ce13671f41689fad87b93f406517
Ray Oh <raymond.oh@gmail.com>
2023-06-06 12:11:51 -0400

    added integration / e2e testing

--- src/page_tracker/app.py
+++ src/page_tracker/app.py
@@ -1,15 +1,21 @@
 from flask import Flask
-from redis import Redis
+from redis import Redis, RedisError
 from functools import cache
+import os
 
 app = Flask(__name__)
 redis = Redis()
 
 @app.get("/")
 def index():
-  page_views = redis().incr("page_views")
-  return f"This page has been viewed {page_views} times."
+  try:
+    page_views = redis().incr("page_views")
+  except RedisError:
+    app.logger.exception("Redis error")
+    return "Sorry, something went wrong \N{pensive face}", 500
+  else:
+    return f"This page has been viewed {page_views} times."
 
 @cache
 def redis():
-  return Redis()
+  return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
--- test/unit/test_app.py
+++ test/unit/test_app.py
@@ -1,13 +1,24 @@
 import pytest
 import unittest.mock
-
+from redis import ConnectionError
 from page_tracker.app import app
-from flask import request
 
-@pytest.fixture
-def http_client():
-    return app.test_client()
 
+# @pytest.fixture
+# def http_client():
+#     return app.test_client()
+
+@unittest.mock.patch("page_tracker.app.redis")
+def test_should_handle_redis_connection_error(mock_redis, http_client):
+    # Given
+    mock_redis.return_value.incr.side_effect = ConnectionError
+
+    # When
+    response = http_client.get("/")
+
+    # then
+    assert response.status_code == 500
+    assert response.text == "Sorry, something went wrong \N{pensive face}"
 
 @unittest.mock.patch("page_tracker.app.redis")
 def test_should_call_redis_incr(mock_redis, http_client):
