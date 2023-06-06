0710634cf656b685652bdc057d39faa688174255
Ray Oh <raymond.oh@gmail.com>
2023-06-06 12:35:39 -0400

    included libraries for static checks; ran them manually

--- src/page_tracker/app.py
+++ src/page_tracker/app.py
@@ -1,21 +1,22 @@
+import os
+from functools import cache
+
 from flask import Flask
 from redis import Redis, RedisError
-from functools import cache
-import os
 
 app = Flask(__name__)
-redis = Redis()
 
 @app.get("/")
 def index():
-  try:
-    page_views = redis().incr("page_views")
-  except RedisError:
-    app.logger.exception("Redis error")
-    return "Sorry, something went wrong \N{pensive face}", 500
-  else:
+    try:
+        page_views = redis().incr("page_views")
+    except RedisError:
+        app.logger.exception("Redis error")
+        return "Sorry, something went wrong \N{pensive face}", 500
+
     return f"This page has been viewed {page_views} times."
+
 
 @cache
 def redis():
-  return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
+    return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
